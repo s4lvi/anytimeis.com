cd65fed30363c7d21e4392b0e3b4dfb3b0a452ea
s4lvi <salvicomputers@gmail.com>
2023-04-08 17:16:07 -0400

    Adding navbar and sections

--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,25 @@
 import React, { useState } from 'react';
 import Tagline from './components/Tagline/Tagline';
 import noise from "./assets/noise.svg"
+import Navbar from './components/Navbar/Navbar';
+import HomeSection from './components/HomeSection';
+import WhyAnytimeSection from './components/WhyAnytimeSection';
+import WhoWeAreSection from './components/WhoWeAreSection';
+import ShowcaseSection from './components/ShowcaseSection';
 
 const Home = () => {
+  const [showTitle, setShowTitle] = useState(false);
+
+  const handleScroll = () => {
+    const yPos = window.pageYOffset;
+    if (yPos > 0) {
+      setShowTitle(true);
+    } else {
+      setShowTitle(false);
+    }
+  };
+
+  window.addEventListener('scroll', handleScroll);
 
   return (
     <div className="text-gray-900">
@@ -10,13 +27,18 @@ const Home = () => {
         className="overlay"
       >
         <img
-        className="noise"
+          className="noise"
           src={noise}
           alt="noise overlay"
           className="absolute inset-0 object-cover w-full h-full opacity-30"
         />
       </div>
-        <Tagline />
+      <Navbar showTitle={showTitle} />
+      <Tagline showTitle={showTitle} />
+      <HomeSection />
+      <WhyAnytimeSection />
+      <WhoWeAreSection />
+      <ShowcaseSection />
     </div>
   );
 };
